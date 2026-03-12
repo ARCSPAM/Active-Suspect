b642c9db0ccb0a0ed4802e154d07c5ba5d551dd1
Alexander Hughes <ashplay20@gmail.com>
2026-03-11 18:42:42 -0600

    Created player, moved camera to player, attempted movement

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCamera : MonoBehaviour
+{
+    private float sens = 100;
+    private Vector2 mouseInput;
+    private float pitch;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, mouseInput.x * sens * Time.deltaTime);
+        pitch -= mouseInput.y * sens * Time.deltaTime;
+        pitch = Mathf.Clamp(pitch, -90f, 90f);
+        transform.localEulerAngles = new Vector3(pitch, transform.localEulerAngles.y, 0f);
+    }
+
+    public void OnMouseMove(InputAction.CallbackContext context)
+    {
+        mouseInput = context.ReadValue<Vector2>();
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform camera;
+
+    private float speed = 15f;
+    private Rigidbody rb;
+    private Vector2 moveInput;
+    
+    public InputActionReference playerMove;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        //Vector3 move = camera.forward * moveInput.y + camera.right * moveInput.x;
+        //move.y = 0f;
+        //rb.AddForce(move.normalized * speed, ForceMode.VelocityChange);
+
+        moveInput = playerMove.action.ReadValue<Vector2>();
+        rb.linearVelocity = new Vector2(moveInput.x * speed, moveInput.y * speed);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        //moveInput = context.ReadValue<Vector2>();
+    }
+}
