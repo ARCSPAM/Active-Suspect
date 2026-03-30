04b22f09d631f81d5bdb78c39ebd5014a49c536d
Alexander Hughes <ashplay20@gmail.com>
2026-03-29 19:31:31 -0600

    Camera movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -28,7 +28,14 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         //Move code
-        Vector3 move = new Vector3(MoveInput.x,0,MoveInput.y);
+        Vector3 forward = transform.forward;
+        Vector3 right = transform.right;
+        forward.y = 0;
+        right.y = 0;
+        forward.Normalize();
+        right.Normalize();
+        Vector3 move = forward * MoveInput.y + right * MoveInput.x;
+        //Vector3 move = new Vector3(MoveInput.x,0,MoveInput.y);
         controller.Move(move*moveSpeed*Time.deltaTime);
 
     }
