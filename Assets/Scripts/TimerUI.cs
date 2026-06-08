2281200399462c9afbaf0120cdc6b398596e654d
Jacob-Chamberlin <112023447+Jacob-Chamberlin@users.noreply.github.com>
2026-06-07 19:38:14 -0600

    TimerUI

---
+++ Assets/Scripts/TimerUI.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class TimerUI : MonoBehaviour
+{
+    [SerializeField] private float initialTime = 60f; //seconds
+    private float currentTime;
+    private Label m_TimerLabel;
+    bool m_Running = true;
+
+    public void OnEnable()
+    {
+        var uiDoc = GetComponent<UIDocument>();
+        m_TimerLabel = uiDoc.rootVisualElement.Q<Label>("TimerLabel");
+
+
+        currentTime = initialTime;
+        updateTimer();
+    }
+
+    private void Update()
+    {
+        if (currentTime > 0 && m_Running)
+        {
+            currentTime -= Time.deltaTime;
+            updateTimer();
+        }
+        else if (currentTime < 0 && m_Running)
+        {
+            currentTime = 0;
+            updateTimer();
+            enabled = false;
+            SceneManager.LoadScene(5);
+        }
+    }
+
+    public void updateTimer()
+    {
+        TimeSpan timeSpan = TimeSpan.FromSeconds(currentTime);
+        m_TimerLabel.text = string.Format("{0:00}:{1:00}", timeSpan.Minutes, timeSpan.Seconds);
+    }
+    public void stopTimer()
+    {
+        m_Running = false;
+    }
+}
